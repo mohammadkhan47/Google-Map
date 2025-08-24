84807850856431402b3d7e2a784a64dcef5a1ec9
Mohammad Khan <mohammadneon47@gmail.com>
2025-08-24 14:22:53 +0500

    add google map

---
+++ lib/home_screen.dart
@@ -0,0 +1,31 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  Completer<GoogleMapController> _controller = Completer();
+  static final CameraPosition _isgoogleFlex = CameraPosition(target: LatLng(37.42796133580664, -122.085749655962),
+  zoom: 14.4746,
+  );
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: GoogleMap(
+          initialCameraPosition: _isgoogleFlex,
+        mapType: MapType.normal,
+        compassEnabled: true,
+        onMapCreated: (GoogleMapController controller){
+            _controller.complete(controller);
+        },
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:googlemaps/home_screen.dart';
+
+void main(){
+  runApp(const MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'google maps',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
