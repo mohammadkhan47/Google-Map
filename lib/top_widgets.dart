46f932e2a45ba902e5aff601e6054ee74de71781
Mohammad Khan <mohammadneon47@gmail.com>
2025-09-05 13:00:44 +0500

    Top widgets

--- lib/top_widgets.dart
+++ lib/top_widgets.dart
@@ -20,162 +20,41 @@ class _TopWidgetsState extends State<TopWidgets> {
         crossAxisAlignment: CrossAxisAlignment.center,
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          //flex is use in expanded widget flex:1 is by default
-         Stack(
-           alignment: Alignment.center,
-           children: [
-             Container(
-               height: 300,
-               width: 140,
-               color: Colors.red,
-             ),
-             Positioned(child: Text('khan'),
-               top: 10,
-             ),
-             Container(
-               height: 130,
-               width: 120,
-               color: Colors.green,
-             ),
-             Container(
-               height: 100,
-               width: 100,
-               color: Colors.yellow,
-             ),
-             Text('mohammad Khan'),
-           ],
-         ),
-          // SizedBox(height: 10),
-          // // Row(
-          // //   children: [
-          // //     Center(
-          // //         child: Container(
-          // //           height: 259,
-          // //           width: 200,
-          // //           // transform: Matrix4.rotationZ(.2),
-          // //           decoration: BoxDecoration(
-          // //             borderRadius: BorderRadius.circular(15),
-          // //             gradient: LinearGradient(colors: [Colors.blueAccent, Colors.pinkAccent],
-          // //                 begin: Alignment.topLeft,
-          // //                 end: Alignment.bottomRight
-          // //             ),
-          // //             color: Colors.black,
-          // //             image: DecorationImage(image: NetworkImage('https://images.pexels.com/photos/1128678/pexels-photo-1128678.jpeg'),
-          // //                 fit: BoxFit.cover
-          // //             ),
-          // //           ),
-          // //           child: Center(child: RichText(text: TextSpan(
-          // //               children: [
-          // //                 TextSpan(
-          // //                   text: 'creating',style: TextStyle(
-          // //                     color: Colors.pinkAccent
-          // //                 ),
-          // //                 ),
-          // //                 TextSpan(
-          // //                   text: 'Container in',style: TextStyle(
-          // //                   color: Colors.teal,
-          // //                 ),
-          // //                 ),
-          // //                 TextSpan(
-          // //                   text: 'flutter',style: TextStyle(
-          // //                     color: Colors.cyan
-          // //                 ),
-          // //                 ),
-          // //               ]
-          // //           ))
-          // //           ),
-          // //         )
-          // //     ),
-          // //     Center(
-          // //       child: Container(
-          // //         height: 259,
-          // //         width: 200,
-          // //         // transform: Matrix4.rotationZ(.2),
-          // //         decoration: BoxDecoration(
-          // //           borderRadius: BorderRadius.circular(15),
-          // //           gradient: LinearGradient(colors: [Colors.blueAccent, Colors.pinkAccent],
-          // //               begin: Alignment.topLeft,
-          // //               end: Alignment.bottomRight
-          // //           ),
-          // //           color: Colors.black,
-          // //           image: DecorationImage(image: NetworkImage('https://images.pexels.com/photos/1128678/pexels-photo-1128678.jpeg'),
-          // //               fit: BoxFit.cover
-          // //           ),
-          // //         ),
-          // //         child: Center(child: RichText(text: TextSpan(
-          // //             children: [
-          // //               TextSpan(
-          // //                 text: 'creating',style: TextStyle(
-          // //                   color: Colors.pinkAccent
-          // //               ),
-          // //               ),
-          // //               TextSpan(
-          // //                 text: 'Container in',style: TextStyle(
-          // //                 color: Colors.teal,
-          // //               ),
-          // //               ),
-          // //               TextSpan(
-          // //                 text: 'flutter',style: TextStyle(
-          // //                   color: Colors.cyan
-          // //               ),
-          // //               ),
-          // //             ]
-          // //         ))
-          // //         ),
-          // //       )
-          // //   )
-          // //   ],
-          // // ),
-          // Expanded(
-          //   flex: 3,
-          //   child: Center(
-          //     child: Container(
-          //       height: 259,
-          //       width: 200,
-          //       // transform: Matrix4.rotationZ(.2),
-          //       decoration: BoxDecoration(
-          //         borderRadius: BorderRadius.circular(15),
-          //         gradient: LinearGradient(colors: [Colors.blueAccent, Colors.pinkAccent],
-          //           begin: Alignment.topLeft,
-          //           end: Alignment.bottomRight
-          //         ),
-          //         color: Colors.black,
-          //         image: DecorationImage(image: NetworkImage('https://images.pexels.com/photos/1128678/pexels-photo-1128678.jpeg'),
-          //             fit: BoxFit.cover
-          //         ),
-          //       ),
-          //       child: Center(child: RichText(text: TextSpan(
-          //         children: [
-          //           TextSpan(
-          //             text: 'creating',style: TextStyle(
-          //             color: Colors.pinkAccent
-          //           ),
-          //           ),
-          //           TextSpan(
-          //             text: 'Container in',style: TextStyle(
-          //               color: Colors.teal,
-          //           ),
-          //           ),
-          //           TextSpan(
-          //             text: 'flutter',style: TextStyle(
-          //               color: Colors.cyan
-          //           ),
-          //           ),
-          //         ]
-          //       ))
+
+          Expanded(child: ListView.builder(
+              itemCount: 100,
+              itemBuilder: (context, index){
+                return ListTile(
+                  title: Text('mohammad khan'),
+                  subtitle: Text('learning flutter'),
+                  trailing: Text('12:44 PM'),
+                  leading: CircleAvatar(
+                    backgroundImage: NetworkImage('https://images.pexels.com/photos/1128678/pexels-photo-1128678.jpeg'),
+                  ),
+                );
+              }))
+
+
+
+          // TextFormField(
+          //   enabled: true,
+          //   decoration: InputDecoration(
+          //     hintText: 'enter email',
+          //     labelText: 'Email',
+          //     prefixIcon: Icon(Icons.email),
+          //     enabledBorder: OutlineInputBorder(
+          //       borderSide: BorderSide(color: Colors.red),
+          //       borderRadius: BorderRadius.circular(10),
           //     ),
-          //   )
-          //   ),
-          // ),
-          // Expanded(
-          //   flex: 3,
-          //   child: Container(
-          //     height: 250,
-          //     width: 250,
-          //     decoration: BoxDecoration(
-          //       color: Colors.cyan,
+          //     focusedBorder: OutlineInputBorder(
+          //       borderSide: BorderSide(color: Colors.green),
+          //       borderRadius: BorderRadius.circular(10),
           //     ),
+          //     errorBorder: OutlineInputBorder(
+          //       borderSide: BorderSide(color: Colors.red)
+          //     )
           //   ),
+          //
           // )
         ],
       ),
